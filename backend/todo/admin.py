5fc58b49ee4707d69a30aa80648c9f7a26c945d0
masa1203 <msys.sawada@gmail.com>
2020-03-10 10:55:25 +0900

    first commit

---
+++ backend/todo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'completed')
+    list_editable = ('title', 'description', 'completed')
+
+# Register your models here.
+
+admin.site.register(Todo, TodoAdmin)
